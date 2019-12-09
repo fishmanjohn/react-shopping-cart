15fafb991f0204214f2f096e6aca3c276d17286c
fishmanjohn <john@emmaandjohnbyvan.com>
2019-12-09 14:30:35 -0800

    on step -3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import {ProductContext}  from './contexts/ProductContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -12,9 +13,9 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart, item])
 	};
-
+	console.log(cart)
 	return (
 		<div className="App">
 			<Navigation cart={cart} />
